e2e358011826fecda709f79a7b73170a2b750d9b
nitish173 <emailnitish22@yahoo.in>
2018-03-19 01:55:49 +0530

    2. Add templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello!"
+    user = {
+        'username': 'nitishhh'
+    }
+    posts = [
+        {
+            'author': 'nitish',
+            'body': 'Lorem ipsum'
+        },
+        {
+            'author': 'ram',
+            'body': 'Lorem ipsum dolores'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
